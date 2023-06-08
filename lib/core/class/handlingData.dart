490b4e96e1a9d52b55779df77d45717d0c5b9ffb
alaaKodsi <alaakodsy.1@gmail.com>
2023-06-08 15:32:28 +0200

    add image.json

---
+++ lib/core/class/handlingData.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:xstore/core/class/StatusRequest.dart';
+import 'package:xstore/core/constant/imageApp.dart';
+
+class HandlingDataView extends StatelessWidget {
+  final StatusRequest statusRequest;
+  final Widget widget;
+  const HandlingDataView(
+      {Key? key, required this.statusRequest, required this.widget})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return statusRequest == StatusRequest.loading
+        ? Center(child: Lottie.asset(ImageApp.loading, width: 250, height: 250))
+        : statusRequest == StatusRequest.offlinefailure
+            ? Center(
+                child:
+                    Lottie.asset(ImageApp.disconnect, width: 250, height: 250))
+            : statusRequest == StatusRequest.serverfailure
+                ? Center(
+                    child: Lottie.asset(ImageApp.error404,
+                        width: 250, height: 250))
+                : statusRequest == StatusRequest.failure
+                    ? Center(
+                        child: Lottie.asset(ImageApp.noData,
+                            width: 250, height: 250))
+                    : widget;
+  }
+}
--- lib/core/constant/imageApp.dart
+++ lib/core/constant/imageApp.dart
@@ -1,9 +1,15 @@
 // ignore: file_names
 class ImageApp {
   static const String imageRoot = "assets/images";
+  static const String ainmationRoot = "assets/lottie";
 
   static const String onboardingImageOne = "$imageRoot/onboardingOne.png";
   static const String onboardingImageTwo = "$imageRoot/onboardingTwo.png";
   static const String logo = "$imageRoot/logo.png";
   static const String forgotPassword = "$imageRoot/forgetPassword.png";
+  static const String error404 =
+      "$ainmationRoot/404-website-error-animation.json";
+  static const String loading = "$ainmationRoot/loading.json";
+  static const String disconnect = "$ainmationRoot/disconnect.json";
+  static const String noData = "$ainmationRoot/no-data-animation.json";
 }
