d6112ea19ed6088faa9b01a5860beee7147265e9
alaaKodsi <alaakodsy.1@gmail.com>
2023-06-10 16:02:00 +0200

    signUp connect with Database

--- lib/controller/auth/signUpController.dart
+++ lib/controller/auth/signUpController.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:xstore/core/class/StatusRequest.dart';
+import 'package:xstore/core/functions/handlingData.dart';
+import 'package:xstore/data/datasource/remotly/auth/signup.dart';
 
 abstract class SignUpController extends GetxController {
   signUp();
   goToLogin();
+  showPassword();
 }
 
 class SignUpControllerImp extends SignUpController {
@@ -14,6 +18,9 @@ class SignUpControllerImp extends SignUpController {
   late TextEditingController phone;
   late TextEditingController username;
   bool isShowPassword = true;
+  late StatusRequest statusRequest;
+  Signupdata signupdata = Signupdata(Get.find());
+  List data = [];
 
   @override
   goToLogin() {
@@ -21,35 +28,47 @@ class SignUpControllerImp extends SignUpController {
   }
 
   @override
-  signUp() {
+  signUp() async {
     var formData = formstat.currentState;
     if (formData!.validate()) {
-      return "valid";
-    } else {
-      return "not vaild ";
+      statusRequest = StatusRequest.loading;
+      var response = await signupdata.postData(
+          username.text, password.text, email.text, phone.text);
+      statusRequest = handlingData(response);
+      if (StatusRequest.success == statusRequest) {
+        if (response['status'] == "success") {
+          Get.offAllNamed("/verfiyCode");
+        } else {
+          Get.defaultDialog(
+              title: "ُWarning",
+              middleText: "Phone Number Or Email Already Exists");
+          statusRequest = StatusRequest.failure;
+        }
+      }
+    }
+
+    @override
+    void onInit() {
+      email = TextEditingController();
+      password = TextEditingController();
+      username = TextEditingController();
+      phone = TextEditingController();
+      super.onInit();
+    }
+
+    @override
+    void dispose() {
+      email.dispose();
+      username.dispose();
+      phone.dispose();
+      password.dispose();
+      super.dispose();
     }
   }
 
   @override
-  void onInit() {
-    email = TextEditingController();
-    password = TextEditingController();
-    username = TextEditingController();
-    phone = TextEditingController();
-    super.onInit();
-  }
-
   showPassword() {
     isShowPassword = isShowPassword == true ? false : true;
     update();
-  }
-
-  @override
-  void dispose() {
-    email.dispose();
-    username.dispose();
-    phone.dispose();
-    password.dispose();
-    super.dispose();
   }
 }
---
+++ lib/core/functions/handlingData.dart
@@ -0,0 +1,9 @@
+import 'package:xstore/core/class/StatusRequest.dart';
+
+handlingData(response) {
+  if (response is StatusRequest) {
+    return response;
+  } else {
+    return StatusRequest.success;
+  }
+}
