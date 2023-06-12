7f9ebeacb0a80515eb0b0cb3282675bd1890241a
alaaKodsi <alaakodsy.1@gmail.com>
2023-06-12 16:22:27 +0200

    verfiycod page with API and create Lodaing lottie

--- lib/controller/auth/verfiyCodeSignUpController.dart
+++ lib/controller/auth/verfiyCodeSignUpController.dart
@@ -1,17 +1,42 @@
+import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
+import 'package:xstore/core/class/StatusRequest.dart';
+import 'package:xstore/core/functions/handlingData.dart';
+import 'package:xstore/data/datasource/remotly/auth/verfiyCodeSignup.dart';
 
 abstract class VerifiySignUpController extends GetxController {
-  goToLogin();
+  goToLogin(String verifycodeSignUp);
 }
 
+VerifyCodeSignUp verifyCodeSignUp = VerifyCodeSignUp(Get.find());
+
 class VerifiySignUpControllermp extends VerifiySignUpController {
+  String? email;
+  StatusRequest? statusRequest;
   @override
-  goToLogin() async {
+  goToLogin(verifycodeSignUp) async {
+    statusRequest = StatusRequest.loading;
+    update();
+    var response = await verifyCodeSignUp.postData(email!, verifycodeSignUp);
+    statusRequest = handlingData(response);
+    if (StatusRequest.success == statusRequest) {
+      if (response['status'] == "success") {
+        SchedulerBinding.instance.addPostFrameCallback((_) {
+          Get.offAllNamed("Login");
+        });
+        update();
+      } else {
+        Get.defaultDialog(
+            title: "ُWarning", middleText: "Verify Code not correct");
+        statusRequest = StatusRequest.failure;
+      }
+    }
     Get.offNamed("Login");
   }
 
   @override
   void onInit() {
+    email = Get.arguments['email'];
     super.onInit();
   }
 }
--- lib/view/screen/auth/signUpPage.dart
+++ lib/view/screen/auth/signUpPage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:xstore/core/class/StatusRequest.dart';
 import 'package:xstore/core/constant/color.dart';
 import 'package:xstore/core/functions/exitApp.dart';
 import 'package:xstore/core/functions/validInput.dart';
+import 'package:xstore/view/widget/auth/loading.dart';
 import '../../../controller/auth/signUpController.dart';
 import '../../widget/auth/customElevatedButton.dart';
 import '../../widget/auth/customMoveToSignUporLogin.dart';
@@ -13,8 +15,7 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SignUpControllerImp controller = Get.put(SignUpControllerImp());
-
+    Get.put(SignUpControllerImp());
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
@@ -30,92 +31,94 @@ class SignUpPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: WillPopScope(
-        onWillPop: alertExitApp,
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: controller.formstat,
-            child: ListView(
-              children: [
-                Text(
-                  "Register",
-                  style: Theme.of(context)
-                      .textTheme
-                      .displaySmall!
-                      .copyWith(color: Colors.white),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                CustomTextFormField(
-                  controller: controller.username,
-                  validator: (val) {
-                    return validInput(val!, "username", 9, 30);
-                  },
-                  hint: "Enter your Username",
-                  label: "Username ",
-                  icon: Icons.person_2_outlined,
-                ),
-                CustomTextFormField(
-                  controller: controller.email,
-                  validator: (val) {
-                    return validInput(val!, "email", 10, 30);
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  hint: "Enter your Email",
-                  label: "Email",
-                  icon: Icons.email_outlined,
-                ),
-                CustomTextFormField(
-                  controller: controller.phone,
-                  validator: (val) {
-                    return validInput(val!, "Phone", 9, 15);
-                  },
-                  keyboardType: TextInputType.phone,
-                  hint: "Enter your Phone",
-                  label: "Phone",
-                  icon: Icons.phone_android_outlined,
-                ),
-                GetBuilder<SignUpControllerImp>(
-                  builder: (controller) => CustomTextFormField(
-                    obscureText: controller.isShowPassword,
-                    onTap: controller.showPassword(),
-                    hint: "Enter your Password",
-                    controller: controller.password,
-                    validator: (val) {
-                      return validInput(val!, "Password", 10, 25);
-                    },
-                    label: "Password",
-                    icon: Icons.lock_outline,
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                CustomElevatedButton(
-                  onPressed: () async {
-                    await controller.signUp();
-                  },
-                  text: "Sign up ",
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: Get.height / 3.5,
-                  child: CoustomCheekAccount(
-                    bodyText: " I have an account already !  ",
-                    inkweText: "Login",
-                    onTap: () {
-                      controller.goToLogin();
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+          onWillPop: alertExitApp,
+          child: GetBuilder<SignUpControllerImp>(
+            builder: (controller) =>
+                controller.statusRequest == StatusRequest.loading
+                    ? const Loading()
+                    : Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Form(
+                          key: controller.formstat,
+                          child: ListView(
+                            children: [
+                              Text(
+                                "Register",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .displaySmall!
+                                    .copyWith(color: Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              CustomTextFormField(
+                                controller: controller.username,
+                                validator: (val) {
+                                  return validInput(val!, "username", 9, 30);
+                                },
+                                hint: "Enter your Username",
+                                label: "Username ",
+                                icon: Icons.person_2_outlined,
+                              ),
+                              CustomTextFormField(
+                                controller: controller.email,
+                                validator: (val) {
+                                  return validInput(val!, "email", 10, 30);
+                                },
+                                keyboardType: TextInputType.emailAddress,
+                                hint: "Enter your Email",
+                                label: "Email",
+                                icon: Icons.email_outlined,
+                              ),
+                              CustomTextFormField(
+                                controller: controller.phone,
+                                validator: (val) {
+                                  return validInput(val!, "Phone", 9, 15);
+                                },
+                                keyboardType: TextInputType.phone,
+                                hint: "Enter your Phone",
+                                label: "Phone",
+                                icon: Icons.phone_android_outlined,
+                              ),
+                              CustomTextFormField(
+                                obscureText: controller.isShowPassword,
+                                onTap: controller.showPassword(),
+                                hint: "Enter your Password",
+                                controller: controller.password,
+                                validator: (val) {
+                                  return validInput(val!, "Password", 10, 25);
+                                },
+                                label: "Password",
+                                icon: Icons.lock_outline,
+                              ),
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              CustomElevatedButton(
+                                onPressed: () async {
+                                  await controller.signUp();
+                                },
+                                text: "Sign up ",
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              SizedBox(
+                                height: Get.height / 3.5,
+                                child: CoustomCheekAccount(
+                                  bodyText: " I have an account already !  ",
+                                  inkweText: "Login",
+                                  onTap: () {
+                                    controller.goToLogin();
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+          )),
     );
   }
 }
