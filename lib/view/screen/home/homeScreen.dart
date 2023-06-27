098cac6cad84f3a7e7324ad6e749bef1ad1c936b
alaaKodsi <alaakodsy.1@gmail.com>
2023-06-27 16:30:22 +0200

    add bottomAppBar and controller for it

---
+++ lib/view/screen/home/homeScreen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:xstore/controller/homeScreen_controller.dart';
+import 'package:xstore/core/constant/color.dart';
+import 'package:xstore/view/widget/customAppbar.dart';
+import 'package:xstore/view/widget/home/customButtonAppbar.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Get.put(HomeScreenControllerImp());
+    return GetBuilder<HomeScreenControllerImp>(builder: (controller) {
+      return Scaffold(
+          backgroundColor: backgroundColor,
+          appBar: const CustomAppBar(title: "X Store"),
+          bottomNavigationBar: BottomAppBar(
+            color: const Color.fromARGB(255, 0, 0, 0),
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomButtonAppbar(
+                  icon: Icons.home_outlined,
+                  activ: controller.currentpage == 0 ? true : false,
+                  onPressed: () {
+                    controller.changePage(0);
+                  },
+                ),
+                CustomButtonAppbar(
+                  icon: Icons.notifications_outlined,
+                  activ: controller.currentpage == 1 ? true : false,
+                  onPressed: () {
+                    controller.changePage(1);
+                  },
+                ),
+                CustomButtonAppbar(
+                  icon: Icons.shopping_basket_outlined,
+                  activ: controller.currentpage == 2 ? true : false,
+                  onPressed: () {
+                    controller.changePage(2);
+                  },
+                ),
+                CustomButtonAppbar(
+                  icon: Icons.settings_outlined,
+                  activ: controller.currentpage == 3 ? true : false,
+                  onPressed: () {
+                    controller.changePage(3);
+                  },
+                ),
+              ],
+            ),
+          ),
+          body: controller.listPage.elementAt(controller.currentpage));
+    });
+  }
+}
